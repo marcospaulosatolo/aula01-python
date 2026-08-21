ba948e4b6722021133fab24d5e05f046958cc817
marcospaulosatolo <marcospsatolo132@gmail.com>
2026-08-21 11:07:17 -0300

    Add files via upload

---
+++ aula02hello.py
@@ -0,0 +1,35 @@
+print("Olá, mundo!")
+print("Olá, Marcos!")
+print("10+8")
+print(10+8)
+print(10*8)
+nome="Marcos"
+idade=19
+print("nome:",nome)
+print("idade:",idade)
+print(f"Meu nome é {nome} e tenho {idade} anos.")
+print("Bom dia!\nBem vindo à aula.")
+print("Nome\tNota")
+print("Marcos\t10")
+print("Ana\t8")
+print("21","08","2026", sep="/")
+print("Olá", end=" ")
+print("Marcos!")
+curso="Administração"
+nota="9,5"
+print("====FICHA DO ALUNO====")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Curso:", curso)
+print("Nota:", nota)
+print ("======================")
+nota1=10
+nota2=9
+print("=========================")
+print("Aluno:",nome)
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Média:",(nota1 + nota2)/2)
+print("=========================")
+média=(nota1 + nota2)/2
+print(f"{nome} terminou a atividade com {média}.")
