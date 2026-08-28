cf8e9bccb9e2a47ad3074c7e5144980742e65dc1
marcospaulosatolo <marcospsatolo132@gmail.com>
2026-08-28 11:04:25 -0300

    Add files via upload

---
+++ ex1a4.py
@@ -0,0 +1,20 @@
+nome = "Marcos"
+idade = 19
+cidade = "Piracicaba"
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Cidade: {cidade}")
+produto = "Caderno"
+preco = "25.00"
+quantidade = "100"
+print(f"Produto: {produto}")
+print(f"Preço: {preco}")
+print(f"Quantidade: {quantidade}")
+pontos=10
+print(pontos)
+pontos=20
+print(pontos)
+print(type("Pedro"))
+print(type(25))
+print(type(1.78))
+print(type("2026"))
---
+++ ex5a8.py
@@ -0,0 +1,23 @@
+numero1 = 20
+numero2 = 50
+soma=numero1+numero2
+subtracao=numero1-numero2
+divisao=numero1/numero2
+multiplicacao=numero1*numero2
+print(soma)
+print(subtracao)
+print(divisao)
+print(multiplicacao)
+nota1=9.5
+nota2=9.8
+nota3=10
+media=(nota1 + nota2 + nota3)/3
+print(f"Média:{media}")
+preco = 25.00
+quantidade = 100
+total=preco*quantidade
+print(f"Total da compra={total}")
+salario=5700
+bonus=800
+total=salario+bonus
+print(f"Total a receber={total}")
